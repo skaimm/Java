523700a5d8e1dca3e7e6415cc04ae6000b09c34f
skaimm <ua.smt@hotmail.com>
2019-09-06 19:12:08 +0300

    Simple game
    work on libgdx.

---
+++ forvetistiyoruz/app/src/main/java/com/forvetistiyoruz/forvetistiyoruz/forvetal.java
@@ -0,0 +1,13 @@
+package com.forvetistiyoruz.forvetistiyoruz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class forvetal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forvetal);
+    }
+}
